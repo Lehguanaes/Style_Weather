e7b9287f290c07fadeced4934a04327b0e251164
lehguanaes <lehguanaes@gmail.com>
2025-06-17 14:53:57 -0300

    Adicionando informações das fundadoras

--- src/pages/SobreNos/SobreNos.jsx
+++ src/pages/SobreNos/SobreNos.jsx
@@ -1,22 +1,34 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom'; // Importa o Link do react-router-dom
-import style from './SobreNos.module.css'; // Importa o CSS como módulo
-import logoImg from '../../assets/logo_sobre_nos.png'; // Importe a imagem do logo
+import { Link } from 'react-router-dom';
+import style from './SobreNos.module.css';
+import logoImg from '../../assets/logo_sobre_nos.png';
 import logoResponsiva from '../../assets/logo_responsiva.png';
-import { AppContext } from '../../context/AppContext'; // Importa o contexto global
+import jovanaImg from '../../assets/desenvolvedoras/dev_jovana.jpg';
+import karinneImg from '../../assets/desenvolvedoras/dev_karinne.jpg';
+import leticiaImg from '../../assets/desenvolvedoras/dev_leticia.jpg';
+import mariaImg from '../../assets/desenvolvedoras/dev_maria.jpg';
+import fotoGrupoJuntas from '../../assets/desenvolvedoras/desenvolvedoras.jpg';
+import { AppContext } from '../../context/AppContext';
 
 function SobreNos() {
-  const { usuarioLogado } = useContext(AppContext); // Obtém o estado de autenticação do contexto
+  const { usuarioLogado } = useContext(AppContext);
 
   return (
     <div className={style.container}>
       <div className={style.leftPanel}>
         <div className={style.logoBox}>
-          <img src={logoImg} alt="Style Weather Logo" className={`${style.logoImg} ${style.defaultLogo}`} />
-          <img src={logoResponsiva} alt="Style Weather Logo Responsiva" className={`${style.logoImg} ${style.responsiveLogo}`} />
+          <img
+            src={logoImg}
+            alt="Style Weather Logo"
+            className={`${style.logoImg} ${style.defaultLogo}`}
+          />
+          <img
+            src={logoResponsiva}
+            alt="Style Weather Logo Responsiva"
+            className={`${style.logoImg} ${style.responsiveLogo}`}
+          />
         </div>
       </div>
-
       <div className={style.rightPanel}>
         <div className={style.welcome}>
           <h2 className={style.titulo}>Sobre Nós</h2>
@@ -31,18 +43,47 @@ function SobreNos() {
             conforto, tempo e bem-estar. Seja para um dia chuvoso ou ensolarado, estamos aqui
             para garantir que você esteja pronto, elegante e seguro para qualquer ocasião.
           </p>
-
           <div className={style.contact}>
             <p><strong>Email:</strong> info@styleweather.com</p>
-            <p><strong>Telefone:</strong> (11) 94519-3458</p>
+            <p><strong>Telefone:</strong> (11) 90000-0000</p>
           </div>
-
           <div className={style.signup}>
             {usuarioLogado ? (
               <Link to="../editar-perfil" className={style.signupButton}>Perfil</Link>
             ) : (
               <Link to="../cadastrar" className={style.signupButton}>Cadastre-se</Link>
             )}
+          </div>
+          <div className={style.fundadores}>
+            <h3 className={style.subtitulo}>Quem fundou esse projeto?</h3>
+            <div className={style.fotoGrupo}>
+              <img src={fotoGrupoJuntas} alt="Foto das fundadoras juntas" />
+              <p>
+                Unidas pela vontade de inovar, desenvolvemos este projeto com base em nossas novas experiências e habilidades complementares aprimoradas. Cada detalhe foi pensado com carinho, dedicação e trabalho em equipe.
+              </p>
+            </div>
+            <div className={style.cardContainer}>
+              <div className={style.card}>
+                <img src={jovanaImg} alt="Jovana Oliveira" />
+                <h4>Jovana Oliveira</h4>
+                <p>Desenvolvedora e principal responsável pelo banco de dados e login.</p>
+              </div>
+              <div className={style.card}>
+                <img src={karinneImg} alt="Karinne Angelo" />
+                <h4>Karinne Angelo</h4>
+                <p>Desenvolvedora e principal responsável pela organização dos componentes reutilizáveis.</p>
+              </div>
+              <div className={style.card}>
+                <img src={leticiaImg} alt="Letícia Guanaes" />
+                <h4>Letícia Guanaes</h4>
+                <p>Desenvolvedora e principal responsável pela implementação das API's e lógica.</p>
+              </div>
+              <div className={style.card}>
+                <img src={mariaImg} alt="Maria Monteiro" />
+                <h4>Maria Monteiro</h4>
+                <p>Desenvolvedora e principal responsável pela experiência do usuário.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
